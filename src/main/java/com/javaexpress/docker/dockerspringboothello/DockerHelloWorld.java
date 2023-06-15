901be52293e8944c09e77917d760a96b524e3b69
bouguis <39343543+bouguis@users.noreply.github.com>
2023-06-15 15:31:17 -0400

    Update DockerHelloWorld.java
    test deploye

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -17,6 +17,6 @@ public class DockerHelloWorld {
 	@GetMapping("/aws-test")
 	public String getTest() {
 		
-		return "Welcome to deployment in aws testing";
+		return "Welcome to deployment in aws testing deploye";
 	}
 }
